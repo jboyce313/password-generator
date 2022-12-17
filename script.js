74428649be175f7e88b318e3f5154c7dc90e6eda
Jacob Boyce <jboyce313@gmail.com>
2022-12-17 13:19:32 -0600

    added function to obtain password length

--- script.js
+++ script.js
@@ -7,8 +7,32 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generate password
+var generatePassword = function () {
+  var passwordLength = getPasswordLength();
+};
+
+function getPasswordLength() {
+  var passwordLength = prompt(
+    "Enter password length between 8 and 128 characters: "
+  );
+
+  // check for valid password length entry
+  while (
+    isNaN(passwordLength) ||
+    !Number.isInteger(Number(passwordLength)) ||
+    Number(passwordLength) < 8 ||
+    Number(passwordLength) > 128
+  ) {
+    passwordLength = prompt(
+      "Invalid length. Please enter an integer between between 8 and 128: "
+    );
+  }
+
+  return passwordLength;
+}
